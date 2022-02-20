ede1c156d0ea2e15dcd46e7f925e39d2975143e0
unknown <ilesanmiolushola9@gmail.com>
2022-02-20 10:53:10 +0100

    Shola Portfolio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Hello from './Hello';
+
+function App() {
+  return (
+    <React.Fragment>
+         <Hello />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div>Hi shola</div>
+  )
+}
+
+export default Hello
